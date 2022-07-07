2d4fba83ba80af66eede38f73d49e6776bdc59d8
Ivan <i.zashchyk@gmail.com>
2022-07-07 14:51:26 +0300

    Restored prev version of Program.cs

--- CMD/Program.cs
+++ CMD/Program.cs
@@ -8,15 +8,6 @@ namespace CMD
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("And now I'm going to make a change");
-            string someText = "Hey";
-            Console.WriteLine(someText);
-            HeheImGoing(ref someText);
-            Console.WriteLine(someText);
-        }
-
-        static void HeheImGoing(ref string text)
-        {
-            text = "new text";
         }
     }
 }
