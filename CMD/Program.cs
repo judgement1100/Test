8150c03bb9eedefca3622cf1f13b4a194f6d4630
Ivan <i.zashchyk@gmail.com>
2022-07-07 15:07:45 +0300

    Some changes:
    * Restoring prev Program.cs version

--- CMD/Program.cs
+++ CMD/Program.cs
@@ -7,7 +7,16 @@ namespace CMD
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("some changes");
+            Console.WriteLine("And now I'm going to make a change");
+            string someText = "Hey";
+            Console.WriteLine(someText);
+            HeheImGoing(ref someText);
+            Console.WriteLine(someText);
+        }
+
+        static void HeheImGoing(ref string text)
+        {
+            text = "new text";
         }
     }
 }
